7e82b2ca2a0735a4c959d0ae536c7bd0ddc1e7ca
Arunesh Kumar Dubey <aruneshandroid@gmail.com>
2017-07-25 17:36:02 +0530

    Update BookingApplication.java

--- src/main/java/com/azure/booking/booking/BookingApplication.java
+++ src/main/java/com/azure/booking/booking/BookingApplication.java
@@ -7,12 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BookingApplication {
 
 	public static void main(String[] args) {
-		   /* System.setProperty("http.proxyHost", "pun-proxy");
-	        System.setProperty("http.proxyPort", "3128");
-	        System.setProperty("https.proxyHost", "pun-proxy");
-	        System.setProperty("https.proxyPort", "3128");
-	        System.setProperty("http.proxyUser", "edn3vs");
-	        System.setProperty("http.proxyPassword", "Arun&123");*/
+		 
 		SpringApplication.run(BookingApplication.class, args);
 	}
 }
